b105831afed3eb09e522bae2d9fc85367b8cc15a
NonoLeRobot <sannier.renaud@gmail.com>
2023-07-23 05:14:12 +0200

    safe rayon to prevent double marker in same place

--- front/src/components/Map.svelte
+++ front/src/components/Map.svelte
@@ -9,44 +9,65 @@
   export let isLogged;
 
   onMount(async () => {
+    const securityRadius = 2;
     let AllPoints;
     try {
       const response = await axios.get(
         "http://localhost:5000/api/data/get-all-points"
-      ); // Replace "your_data_endpoint" with the actual API URL
-      AllPoints = response.data; // Assuming the response contains an array of data for markers
+      );
+      AllPoints = response.data;
     } catch (error) {
       console.error("Error fetching data:", error);
     }
-    var customIcon = L.icon({
-      iconUrl: "./assets/elecSpot2.png",
-      iconSize: [50, 50],
-      iconAnchor: [25, 50],
-      shadowSize: [50, 64], // size of the shadow
-      shadowAnchor: [4, 62], // the same for the shadow
-      popupAnchor: [0, -40], // point from which the popup should open relative to the iconAnchor
-    });
 
-    var myLocationIcon = L.icon({
-      iconUrl: `./assets/myPosition.png`,
-      iconSize: [50, 50],
-      iconAnchor: [25, 50],
-      shadowSize: [50, 64], // size of the shadow
-      shadowAnchor: [4, 62], // the same for the shadow
-      popupAnchor: [0, -45], // point from which the popup should open relative to the iconAnchor
-    });
+    const customIcon = createCustomIcon(
+      "./assets/elecSpot2.png",
+      [50, 50],
+      [25, 50],
+      [50, 64],
+      [4, 62],
+      [0, -40]
+    );
+    const myLocationIcon = createCustomIcon(
+      `./assets/myPosition.png`,
+      [50, 50],
+      [25, 50],
+      [50, 64],
+      [4, 62],
+      [0, -45]
+    );
 
-    var map = L.map("map").setView([51.505, -0.09], 13);
+    const map = L.map("map").setView([51.505, -0.09], 13);
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       maxZoom: 19,
       attribution:
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(map);
 
-    map.locate({ setView: true }); //maxZoom: 16
+    map.locate({ setView: true });
+
+    const existingMarkers = [];
+
+    function createCustomIcon(
+      iconUrl,
+      iconSize,
+      iconAnchor,
+      shadowSize,
+      shadowAnchor,
+      popupAnchor
+    ) {
+      return L.icon({
+        iconUrl,
+        iconSize,
+        iconAnchor,
+        shadowSize,
+        shadowAnchor,
+        popupAnchor,
+      });
+    }
 
     function onLocationFound(e) {
-      var radius = e.accuracy;
+      const radius = e.accuracy;
 
       L.marker(e.latlng, { icon: myLocationIcon })
         .addTo(map)
@@ -63,40 +84,42 @@
 
     map.on("locationerror", onLocationError);
 
-    AllPoints.forEach((point) => {
-      let pointLat = point.coords.lat;
-      let pointLng = point.coords.lng;
-      console.log(pointLat, pointLng);
-      var marker = L.marker([pointLat, pointLng], { icon: customIcon }).addTo(
-        map
+    function onMapClick(e) {
+      const popup = L.popup();
+      const container = document.createElement("div");
+
+      const yesButton = document.createElement("button");
+      yesButton.textContent = "oui";
+      yesButton.addEventListener("click", handleYesButtonClick);
+
+      const noButton = document.createElement("button");
+      noButton.textContent = "non";
+      noButton.addEventListener("click", handleNoButtonClick);
+
+      container.appendChild(
+        document.createTextNode("Voulez-vous placer un point ici ?")
       );
-      marker.bindPopup(
-        `<b>${point.pointName}</b><br>${point.pointDescription}`
-      );
+      container.appendChild(yesButton);
+      container.appendChild(noButton);
 
-      function onMapClick(e) {
-        var popup = L.popup();
-        var container = document.createElement("div");
+      popup.setLatLng(e.latlng).setContent(container).openOn(map);
 
-        var yesButton = document.createElement("button");
-        yesButton.textContent = "oui";
-        yesButton.addEventListener("click", handleYesButtonClick);
+      async function handleYesButtonClick() {
+        const existsWithinRadius = existingMarkers.some((markerPos) => {
+          const distance = e.latlng.distanceTo(markerPos.latlng);
+          return distance <= markerPos.radius;
+        });
 
-        var noButton = document.createElement("button");
-        noButton.textContent = "non";
-        noButton.addEventListener("click", handleNoButtonClick);
+        if (existsWithinRadius) {
+          alert("Une prise existe déjà à cet endroit");
+          map.closePopup();
+          return;
+        }
 
-        container.appendChild(
-          document.createTextNode("Voulez vous placer un point ici ?")
-        );
-        container.appendChild(yesButton);
-        container.appendChild(noButton);
-
-        popup.setLatLng(e.latlng).setContent(container).openOn(map);
-
-        async function handleYesButtonClick() {
-          await axios
-            .post("http://localhost:5000/api/data/add-point", {
+        try {
+          const res = await axios.post(
+            "http://localhost:5000/api/data/add-point",
+            {
               email: localStorage.getItem("email"),
               pointName: "point xxx",
               pointDescription: "point description",
@@ -106,35 +129,54 @@
               },
               addedBy: localStorage.getItem("userId"),
               addedDate: new Date(),
-            })
-            .then((res) => {
-              console.log(res);
+            }
+          );
 
-              const point = res.data.point;
-              console.log(point);
+          const point = res.data.point;
 
-              var marker = L.marker([point.coords.lat, point.coords.lng], {
-                icon: customIcon,
-              }).addTo(map);
-              marker.bindPopup(
-                `<b>${point.pointName}</b><br>${point.pointDescription}`
-              );
-              //ajouter un custom icon pour le point ajouté
-              //ajouter un popup pour le point ajouté
+          const marker = L.marker([point.coords.lat, point.coords.lng], {
+            icon: customIcon,
+          }).addTo(map);
 
-              map.closePopup();
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-        }
+          marker.bindPopup(
+            `<b>${point.pointName}</b><br>${point.pointDescription}`
+          );
 
-        function handleNoButtonClick() {
+          existingMarkers.push({
+            latlng: e.latlng,
+            radius: securityRadius, // Change this value to the desired radius of the circle
+          });
+
+          L.circle(e.latlng, securityRadius).addTo(map); // Add circle around the new marker
+
           map.closePopup();
+        } catch (err) {
+          console.log("Error adding point:", err);
         }
       }
 
-      map.on("contextmenu", onMapClick);
+      function handleNoButtonClick() {
+        map.closePopup();
+      }
+    }
+
+    map.on("contextmenu", onMapClick);
+
+    AllPoints.forEach((point) => {
+      const marker = L.marker([point.coords.lat, point.coords.lng], {
+        icon: customIcon,
+      }).addTo(map);
+
+      marker.bindPopup(
+        `<b>${point.pointName}</b><br>${point.pointDescription}`
+      );
+
+      existingMarkers.push({
+        latlng: L.latLng(point.coords.lat, point.coords.lng),
+        radius: securityRadius, // Change this value to the desired radius of the circle
+      });
+
+      L.circle([point.coords.lat, point.coords.lng], securityRadius).addTo(map); // Add circle around existing marker
     });
   });
 </script>
@@ -151,10 +193,7 @@
 {/if}
 
 <style>
-  #map-section-logged {
-    height: 100vh;
-    width: 100vw;
-  }
+  #map-section-logged,
   #map-section {
     height: 100vh;
     width: 100vw;
